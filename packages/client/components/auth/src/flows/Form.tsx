fb8e07c5aaf054e728b042b7a5705798a441e660
Jacob Schlecht <dadadah@echoha.us>
2026-03-09 22:40:56 -0600

    fix: Add lint disabling as value is expected to be an any here
    This commit was made without the use of generative AI.
    
    Signed-off-by: Jacob Schlecht <dadadah@echoha.us>

--- packages/client/components/auth/src/flows/Form.tsx
+++ packages/client/components/auth/src/flows/Form.tsx
@@ -87,6 +87,7 @@ export function Fields(props: FieldProps) {
     <For each={props.fields}>
       {(field) => {
         let fieldName: Field;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         let value: any;
         let disabled: boolean | undefined;
         if (typeof field === "string") {
