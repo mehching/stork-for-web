f5c770f386bd799c627cdde58aad75a173891635
mehching <your@email.com>
2026-03-10 16:00:53 +0000

    Lighten channel bar, main chat, member bar one shade, add border to main chat

--- packages/client/components/ui/components/layout/Main.tsx
+++ packages/client/components/ui/components/layout/Main.tsx
@@ -19,6 +19,8 @@ export const main = cva({
     marginInline: "var(--gap-md)",
     marginBlockEnd: "var(--gap-md)",
     borderRadius: "var(--borderRadius-xl)",
-    background: "var(--md-sys-color-surface-container-low)",
+    background: "var(--md-sys-color-surface-container)",
+    border: "1px solid var(--md-sys-color-outline-variant)",
+    border: "1px solid var(--md-sys-color-outline-variant)",
   },
 });
